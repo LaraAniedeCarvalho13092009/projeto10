1612c8f44557885694c96182d46f14a9e9ee99fa
LaraAniedeCarvalho13092009 <107076724+LaraAniedeCarvalho13092009@users.noreply.github.com>
2022-07-20 17:48:35 -0300

    Add files via upload

---
+++ projeto10/sketch.js
@@ -0,0 +1,55 @@
+var sea,ship;
+var seaImg,shipImg;
+var ship1;
+var ship2; 
+
+
+
+function preload(){
+  seaImg = loadImage("sea.png");
+  shipImg1 = loadAnimation("ship-1.png","ship-2.png");
+  
+  
+  seaImg.addImage(seaImg);
+
+  ship.addAnimation("ship-1.png","ship-2.png")
+
+  if (seaImg.x < 0) {
+    seaImg.x = sea.width/2;
+  }
+
+  }
+
+
+
+
+
+function setup(){
+  createCanvas(400,400);
+  background("blue");
+
+  // Plano de fundo móvel
+  sea=createSprite(400,200);
+  sea.addImage(seaImg);
+  sea.velocityX = -5;
+  sea.scale=0.3;
+
+  
+  ship = createSprite(130,200,30,30);
+  ship.addAnimation("movingShip",shipImg1);
+  ship.scale =0.25;
+  
+}
+
+function draw() {
+  background(0);
+  sea.velocityX = -3;
+
+  
+  //código para redefinir o plano de fundo
+  if(sea.x < 0){
+    sea.x = sea.width/8;
+  }
+    
+  drawSprites();
+}
